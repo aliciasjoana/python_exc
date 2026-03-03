7bf567cf72b9397357c4f3b73173f74a6f45cc0c
Alícia Siqueira Joana <alicia.joana@aluno.senai.br>
2026-03-02 22:47:47 -0300

    exercicio12.py

---
+++ exercicio12.py
@@ -0,0 +1,17 @@
+dia = int(input("Digite um numero"))
+if (dia == 1):
+    print("Domingo")
+elif (dia == 2):
+    print("Segunda")
+elif (dia == 3):
+    print("Terça")
+elif (dia == 4):
+    print("Quarta")
+elif (dia == 5):
+    print("Quinta")
+elif (dia == 6):
+    print("Sexta")
+elif (dia == 7):
+    print("Sábado")
+if (dia != 1 and dia != 2 and dia != 3 and dia != 4 and dia != 5 and dia != 6 and dia != 7):
+    print("Valor inválido")
