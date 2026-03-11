8d707786b5c625759e2fabe9a902f3d611175395
Alícia Siqueira Joana <alicia.joana@aluno.senai.br>
2026-03-11 08:49:34 -0300

    exer2.py

---
+++ exer2.py
@@ -0,0 +1,9 @@
+nome = (input("Digite seu nome"))
+senha = (input("Digite sua senha"))
+while nome == senha:
+    print("Sua senha não pode ser seu usuario")
+    print("Digite suas informaçoes novamente")
+#Usado para repetir a estrutura do codigo
+    nome = (input("Digite seu nome"))
+    senha = (input("Digite sua senha"))
+    print('Olá',nome, 'seja bem-vindo')
